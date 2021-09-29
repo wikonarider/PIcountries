f1a038f13bed06ace5d63ec6fabefac2977374f7
wikonarider <wikonarider@gmail.com>
2021-09-29 14:39:50 -0300

    rutes activities, countries

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,10 @@
 const { Router } = require('express');
+const { getCountries } = require('../controllers/controllerCountry');
+const {     
+    createActivity,
+    deleteActivity,
+    getAllActivities, 
+} = require('../controllers/controllerActivity');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +13,15 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+// GET /countries
+router.get('/countries', getCountries);
+router.get('/countries/:id', getCountries);
+router.get('/countries/?name=', getCountries);
+router.post('/add', createActivity);
+// EXTRAS -----------------------------------------
+router.delete('/delete/:id', deleteActivity);
+router.get('/activities', getAllActivities);   
+
 
 
 module.exports = router;

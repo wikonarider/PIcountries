db9c6fb1ab2a9e69dd2191155e737e2ce895b3be
wikonarider <wikonarider@gmail.com>
2021-09-29 14:36:23 -0300

    models db

---
+++ api/src/models/Activity.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('activity', {
+    name: {
+        type: DataTypes.STRING,
+    },
+    difficulty: {
+        type: DataTypes.ENUM("1", "2", "3", "4", "5"),
+    },
+    duration: {
+        type: DataTypes.FLOAT,
+    },
+    season: {
+        type: DataTypes.ENUM("summer", "autumn", "winter", "spring"),
+    }
+  }, {
+      timestamps: false,
+  });
+};
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,40 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
-    name: {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+    },
+    cod: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    flag: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    continent: {
+      type:DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type:DataTypes.STRING,
+      allowNull: false,
+    },
+    region: {
+      type:DataTypes.STRING,
+      allowNull: false,
+    },
+    area: {
+      type:DataTypes.FLOAT,
+    },
+  }, {
+    timestamps: false // PARA QUE NO MUESTRE EN CONSOLA CUANDO ENTRA O SALE
   });
 };

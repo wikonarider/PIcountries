99d98948948040e9b29e5ee5a97fe4aa635e9fe9
wikonarider <wikonarider@gmail.com>
2021-10-03 20:55:05 -0300

    filters done

---
+++ client/src/components/Filters/Filters.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { filterByContinent, 
+        orderBy,
+        orderByActivity
+     } from '../../redux/actions';
+
+const Filters = () => {
+    const dispatch = useDispatch();
+
+    const activities = useSelector(state => state.allActivities)
+
+    const handleOnContinent = (e) => {
+    dispatch(filterByContinent(e.target.value))
+    };
+
+    const handleOnOrder = (e) => {
+        dispatch(orderBy(e.target.value));
+    };
+
+    const handleOnActivity = (e) => {
+        dispatch(orderByActivity(e.target.value));
+    };
+
+    return (
+        <div>
+            <label>CONTINENT</label>
+            <select onChange={handleOnContinent} name="" id="">
+                <option value="">-</option>
+                <option value="Africa">Africa</option>
+                <option value="Americas">Americas</option>
+                <option value="Asia">Asia</option>
+                <option value="Europe">Europe</option>
+                <option value="Oceania">Oceania</option>
+            </select>
+            <label>ABC</label>
+            <select value onChange={handleOnOrder} name="order" id="">
+                <option value="">-</option>
+                <option value="ASC">A-Z</option>
+                <option value="DES">Z-A</option>
+            </select>
+            <label>AREA</label>
+            <select value onChange={handleOnOrder} name="area" id="">
+                <option value="">-</option>
+                <option value="MAX">biggest</option>
+                <option value="MIN">smaller</option>
+            </select>
+            <label>ACTIVITY</label>
+            <select onChange={handleOnActivity} name="activity" id="">
+            <option value="">-</option>
+                {
+                   activities.map(a => (
+                                <option key={a.id} value={a.name}>{a.name}</option>
+                                ))
+                }
+
+            </select>
+        </div>
+    );
+};
+
+export default Filters;

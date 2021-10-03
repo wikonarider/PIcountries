b87c838ab17f3bd3e8e2cf3437d021a900158c53
wikonarider <wikonarider@gmail.com>
2021-10-03 20:56:25 -0300

    component done

---
+++ client/src/Nav/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory  } from 'react-router';
+import { getAllCountries } from '../redux/actions';
+
+const SearchBar = () => {
+    const history = useHistory();
+    const [ name, setName ] = useState('');
+    const dispatch = useDispatch();
+
+    const handleOnClick = () => {
+        dispatch(getAllCountries(name));
+        setName('');
+        history.push(`/main?name=${name}`);
+    };
+
+
+    return(
+        <div>
+            <input onChange={({ target: {value} }) => setName(value)} value={name} type='text' placeholder='country name...'></input> 
+            <button onClick={handleOnClick}>Search</button>
+        </div>
+    );
+};
+
+export default SearchBar;

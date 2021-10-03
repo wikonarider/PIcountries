bca0d988898fc3c22ec062339d812331c45e01f2
wikonarider <wikonarider@gmail.com>
2021-10-03 20:51:11 -0300

    provider and store done

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+import store from './redux/store';
+import { Provider } from 'react-redux';
+axios.defaults.baseURL = 'http://localhost:3001/';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
+  
   document.getElementById('root')
 );
 

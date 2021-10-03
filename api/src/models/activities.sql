5ee7913cb8ff97fa9fa51e71269c615a07a9bd32
wikonarider <wikonarider@gmail.com>
2021-10-03 20:48:18 -0300

    activities examples

---
+++ api/src/models/activities.sql
@@ -0,0 +1,49 @@
+{
+                "name": "visit to the temple",
+                "difficulty": "2",
+                "duration": "5",
+                "season": "spring",
+                "country": ["India"]
+}
+{
+                "name": "Trekking",
+                "difficulty": "3",
+                "duration": "5",
+                "season": "spring",
+                "country": ["Argentina"]
+}
+{
+                "name": "Trekking",
+                "difficulty": "3",
+                "duration": "5",
+                "season": "spring",
+                "country": ["Chile"]
+}
+{
+                "name": "Road of the gods",
+                "difficulty": "2",
+                "duration": "4",
+                "season": "spring",
+                "country": ["Greece"]
+}
+{
+                "name": "Dance",
+                "difficulty": "1",
+                "duration": "5",
+                "season": "summer",
+                "country": ["France"]
+}
+{
+                "name": "Gastronomic route",
+                "difficulty": "1",
+                "duration": "2",
+                "season": "autumn",
+                "country": ["Perú"]
+}
+{
+                "name": "Wine rute",
+                "difficulty": "1",
+                "duration": "5",
+                "season": "winter",
+                "country": ["Argentina"]
+}

6abd12bbd91f1aa12968b6bf8eb38825e5f3e700
Miki <micaela.mntr@gmail.com>
2022-04-20 22:51:16 -0300

    update style

--- api/index.js
+++ api/index.js
@@ -20,6 +20,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+// true --> restart db 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {

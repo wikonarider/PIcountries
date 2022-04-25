50f5add1c29e6e8fb5b3061e894e62d040b7ba3b
Miki <micaela.mntr@gmail.com>
2022-04-25 13:22:44 -0300

    install and require and use cors

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,8 @@ const cookieParser = require('cookie-parser');
 // const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-
+// cors for deploy
+const cors= require("cors");
 require('./db.js');
 
 const server = express();
@@ -16,12 +17,14 @@ server.use(express.json())
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from. For deploy this has to match with any domain "*".
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
+
+server.use(cors());
 
 server.use('/', routes);
 
